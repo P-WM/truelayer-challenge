7624bbe1a5fca1c7b6de3813febb1e9a91ffb190
Peter Warner-Medley <peter.warnermedley@googlemail.com>
2020-04-27 03:01:27 +0100

    Match to articles slightly better

--- test/test_enwiki_matcher.py
+++ test/test_enwiki_matcher.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
-from pyspark.sql.types import
+from pyspark.sql import SparkSession, Row
+
+from truelayer_challenge.enwiki_matcher import EnwikiMatcher
+
+from test.fixtures import articles, processed_movies
 
 
 class TestEnwikiMatcher(TestCase):
@@ -8,13 +12,32 @@ class TestEnwikiMatcher(TestCase):
         cls.spark = SparkSession.builder.master("local[*]").appName(
             "TestEnwikiMatcher").getOrCreate()
 
-    def test_matches_film_pages_preferentially(self):
-        pass
+        cls.test_articles = cls.spark.createDataFrame(articles)
 
-    def test_does_not_drop_movies_with_no_article(self):
+    def setUp(self):
+        self.test_matcher = EnwikiMatcher(data=self.test_articles)
+
+    def test_matches_film_pages_preferentially(self):
+        movies_df = self.spark.createDataFrame(processed_movies)
+        actual_articles = self.test_matcher.join_with_movies(movies_df).select(
+            'movie_id', 'url').collect()
+
+        expected_articles = [
+            Row(movie_id='FFFFFFFFBF6AD93F',
+                url='https://en.wikipedia.org/wiki/Executive_Decision'),
+            Row(movie_id='752E4DDB',
+                url=
+                'https://en.wikipedia.org/wiki/Anywhere_but_Here_(1999 film)'),
+            Row(movie_id='FFFFFFFFB28C5FBD',
+                url='https://en.wikipedia.org/wiki/Shalako_(film)'),
+            Row(movie_id='46A353C9',
+                url='https://en.wikipedia.org/wiki/The_Strangers_(2008_film)')
+        ]
+
+        self.assertCountEqual(actual_articles, expected_articles)
+
+    def test_ignores_case(self):
         pass
 
     def test_tidies_names(self):
         pass
-
-
--- truelayer_challenge/enwiki_matcher.py
+++ truelayer_challenge/enwiki_matcher.py
@@ -1,6 +1,6 @@
 from pyspark.sql import DataFrame
 
-from pyspark.sql.functions import regexp_extract, concat
+from pyspark.sql.functions import regexp_extract, col, concat, lit, lower
 
 
 class EnwikiMatcher:
@@ -10,8 +10,9 @@ class EnwikiMatcher:
 
     @staticmethod
     def _clean_articles(articles: DataFrame) -> DataFrame:
-        return articles.withColumn(
-            regexp_extract(articles.title, r"^Wikipedia: (.+)", 1))
+        return articles \
+            .withColumn('wiki_title', regexp_extract(articles.title, r"^Wikipedia: (.+)", 1)) \
+            .drop('title')
 
     @property
     def all_articles(self):
@@ -35,19 +36,32 @@ class EnwikiMatcher:
         the title: "<movie_title> (film)"
 
         Clearly, a much better rule set could be used here that used other information
-        such as release date, country, language etc.
+        such as country, language etc.
 
         It would also be sensible to preprocess the articles, pick out those on films
-        and surface useful metadata for matching.
+        and surface useful metadata for matching. We could then give some useful hints to speed things up
+        by partitioning by the info we wanted.
         """
-        # First pick all articles explicitly about films
+        movies = movies.select('title', 'year', 'movie_id')
+
+        # First try articles about a film in that year
+        correct_year_film_articles = self.all_articles \
+            .join(movies, lower(self.all_articles.wiki_title) == lower(concat(movies.title, lit(' ('), movies.year, lit(' film)')))) \
+            .select('movie_id', 'abstract', 'url', 'wiki_title', 'title', 'year')
+
+        # Next add all articles explicitly about films
         explicitly_film_articles = self.all_articles \
-            .join(movies, self.all_articles.title == concat(movies.title, ' (film)'))
+            .join(movies, lower(self.all_articles.wiki_title) == lower(concat(movies.title, lit(' (film)')))) \
+            .join(correct_year_film_articles, 'movie_id', 'left_anti') \
+            .union(correct_year_film_articles)
 
-        # Add all articles with the same name as movies (but not in the above)
+        # Finally add all articles with the same name as movies (but not in the above)
         same_title_articles = self.all_articles \
-            .join(movies, 'title') \
-            .join(explicitly_film_articles, 'movie_id', 'left_anti')
+            .join(movies, lower(self.all_articles.wiki_title) == lower(movies.title)) \
+            .join(explicitly_film_articles, 'movie_id', 'left_anti') \
+            .union(explicitly_film_articles)
 
-        return movies \
-            .join(same_title_articles, 'movie_id', 'left')
+        all_film_articles = same_title_articles.select('movie_id', 'url',
+                                                       'abstract')
+
+        return all_film_articles
